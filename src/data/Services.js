3009a1656efc87cdd644eea27313808db7c52172
Mohamedabdullah222 <mohammedapdala12@gmail.com>
2025-09-20 17:03:18 +0300

    portfolio 1.0

--- src/data/Services.js
+++ src/data/Services.js
@@ -1,21 +1,21 @@
 export const services = [
   {
     id: 1,
-    name: 'Web design',
+    name: 'Ferrari-Cars',
     icon: './assets/web-icon.png',
     description: 'Web development is the process of building, programming...',
     link: 'https://github.com/Mohamedabdullah222/Ferrari-Cars',
   },
   {
     id: 2, 
-    name: 'Web design',
+    name: 'E-Shop',
     icon: './assets/web-icon.png',
     description: 'Web development is the process of building, programming...',
     link: 'https://github.com/Mohamedabdullah222/E-Shop',
   },
   {
     id: 3, 
-    name: 'Web design',
+    name: 'Portfolio-One',
     icon: './assets/web-icon.png',
     description: 'Web development is the process of building, programming...',
     link: 'https://github.com/Mohamedabdullah222/portfolio',
--- src/data/Work.js
+++ src/data/Work.js
@@ -3,21 +3,21 @@ export const work = [
     id: 1,
     name: 'Frontend project',
     icon: './assets/work-1.png',
-    description: ' Web Design',
+    description: ' Web Design / E-Shop',
     link: 'https://eshop-one2025.vercel.app/',
   },
   {
     id: 2,
     name: 'Frontend project',
     icon: './assets/work-4.PNG',
-    description: 'Web Design',
+    description: 'Web Design / Ferrari-Cars',
     link: 'https://ferrari-cars.vercel.app/',
   },
   {
     id: 3,
     name: 'Frontend project',
     icon: './assets/work-5.PNG',
-    description: 'Web Design',
+    description: 'Web Design / Portfolio',
     link: 'https://portfolio-mohamedabdullah.vercel.app/',
   }
 ];
